2bc2a0832a890786e9b0d960ed5731efb41c79bc
sethberlin <sethberlin@gmail.com>
2014-06-21 00:18:03 -0500

    cachematrix.R
    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Purpose of this code is to create a matrix and inverse it.
+## If the matrix has been inversed already, it will give a "Getting Cached Data"
+## Message.
 
-## Write a short comment describing this function
+##This function creates the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
 
+##Here I am setting the matrix  
+  set <- function(y) {
+    x <<- y
+    return(y)
+    m <<- NULL
+  }
+## Now I get the matrix  
+
+  get <- function() x
+  setInverse <- function(mean) m <<- mean
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+##Now test to see if inverse has been created.
+##If created, I will return a "getting cached data" message.
+##If not create, I use the "solve" function to inverse the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setInverse(m)
+  m
 }
